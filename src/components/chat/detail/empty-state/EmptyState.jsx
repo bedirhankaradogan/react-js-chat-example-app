96c8ef6020ac33253092e85548a08d4574330290
Bedirhan Karadoğan <karadogan.bedirhan@gmail.com>
2018-08-05 22:27:04 +0300

    Add responsiveness

--- src/components/chat/detail/empty-state/EmptyState.jsx
+++ src/components/chat/detail/empty-state/EmptyState.jsx
@@ -9,7 +9,7 @@ export default class EmptyState extends Component {
     const {user} = this.props;
 
     return(
-      <div className={'chat-detail-empty-state'}>
+      <div className={'chat-detail-empty-state box-sizing'}>
         <div className={'chat-detail-empty-state-avatar box-sizing'} style={{backgroundImage: 'url(' + user.profilePicture + ')'}}></div>
         <span className={'chat-detail-empty-state-text chat-detail-empty-state-text-greeting'}>Welcome <b>{user.name}</b></span>
         <span className={'chat-detail-empty-state-text chat-detail-empty-state-text-instruction'}>Select a user and start conversation!</span>
